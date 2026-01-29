d433f7592fa86661dfbfc5403a26af53ce6dde35
Mr Pepsi <MrPepsi1069@gmail.com>
2026-01-28 16:23:35 -0800

    fix

--- commands/mutevc.js
+++ commands/mutevc.js
@@ -1,7 +1,7 @@
 // commands/mutevc.js
 const { SlashCommandBuilder } = require('discord.js');
 const { successEmbed, errorEmbed } = require('../utils/embeds');
-const { hasStaffPerms } = require('../utils/permissions');
+const { hasStaffPerms, hasManagerPerms } = require('../utils/permissions');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -28,19 +28,35 @@ module.exports = {
         }
 
         let mutedCount = 0;
+        let skippedCount = 0;
+        
         for (const [, member] of voiceChannel.members) {
-            if (!member.voice.serverMute && member.id !== interaction.client.user.id) {
-                try {
-                    await member.voice.setMute(true);
-                    mutedCount++;
-                } catch (err) {
-                    console.log(`Could not mute ${member.user.tag}`);
+            // Skip if: already muted, is the bot, is the command user, or has manager perms
+            if (member.voice.serverMute || 
+                member.id === interaction.client.user.id || 
+                member.id === interaction.user.id ||
+                await hasManagerPerms({ member, guild: interaction.guild })) {
+                if (!member.voice.serverMute && member.id !== interaction.client.user.id) {
+                    skippedCount++;
                 }
+                continue;
+            }
+
+            try {
+                await member.voice.setMute(true);
+                mutedCount++;
+            } catch (err) {
+                console.log(`Could not mute ${member.user.tag}`);
             }
         }
 
+        let responseText = `Successfully muted **${mutedCount}** member(s) in ${voiceChannel.name}`;
+        if (skippedCount > 0) {
+            responseText += `\n*Skipped ${skippedCount} staff/manager(s)*`;
+        }
+
         await interaction.reply({
-            embeds: [successEmbed('Voice Channel Muted', `Successfully muted **${mutedCount}** member(s) in ${voiceChannel.name}`)]
+            embeds: [successEmbed('Voice Channel Muted', responseText)]
         });
     }
 };
