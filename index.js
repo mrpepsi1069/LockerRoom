39ba42e97a89e13eb1643e21b59701aced37c18c
Mr Pepsi <MrPepsi1069@gmail.com>
2026-01-29 19:55:18 -0800

    fix website

--- index.js
+++ index.js
@@ -150,6 +150,13 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify({ error: 'Not found' }));
 });
 
+// Serve website
+if (req.url === '/' || req.url === '/index.html') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(fs.readFileSync('./lockerroom-website.html', 'utf8'));
+  return;
+}
+
 server.listen(PORT, () => {
     console.log(`🌐 HTTP server on ${PORT}`);
 });
